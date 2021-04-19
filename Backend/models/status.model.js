ce560de74ad0214789468989662118365df79167
Manil S. Malla <manilsmalla@gmail.com>
2021-04-19 19:18:57 +0545

    Created Status model.

---
+++ Backend/models/status.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const statusSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Status name is mandatory."],    
+        unique: [true, "Status already exists."],
+        minlength: [1, "Status name cannot be blank."],
+        maxlength: [32, "Status name must be 32 characters at maximum."],
+    },
+    description: String,
+    familyname: Schema.Types.Mixed,         //  Either this,
+    // familyname: mongoose.Mixed,           //  Or this.
+   
+    
+}, {
+    timestamps: true,   //  Adds createdAt and modifiedAt fields
+});
+
+const Status = mongoose.model('Status', statusSchema);
+
+module.exports = Status;
