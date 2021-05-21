2cef8d89b3528f8a1a4b0d0ccb20176dc28df1cd
Manil S. Malla <manilsmalla@gmail.com>
2021-05-21 17:34:16 +0545

    Add 'NavBar' component to the 'App'.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import NavBar from './components/NavBar.component';
 import TaskCard from './components/TaskCard.component';
+
 
 import {ThemeProvider } from '@material-ui/core/styles';
 import {mainTheme} from './components/shared/Theme'
@@ -11,6 +13,7 @@ import {mainTheme} from './components/shared/Theme'
 function App() {
   return(
     <ThemeProvider theme= {mainTheme}>
+      <NavBar />
       <TaskCard title= "Jogging"/>
     </ThemeProvider>
       
