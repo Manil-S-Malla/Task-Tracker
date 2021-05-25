3699dc151fcb3c72b049509478a5685a9c3b5cf8
Manil S. Malla <manilsmalla@gmail.com>
2021-05-25 12:06:47 +0545

    Create 'TaskCardHolder' component.

---
+++ src/components/TaskCardHolder.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {TaskCardHolderMaxWidth as maxWidth} from './Shared/CONST';
+
+const TaskCardHolder = (props) => {
+    return(
+        <div 
+            style= {{
+                backgroundColor: '#F4F5F7',
+                width: maxWidth,
+                justifyContent: 'center',
+                borderRadius: 5,
+                flexDirection: 'column',
+                margin: 10,
+            }}
+        >
+           {console.log(props.children)}
+           {props.children}
+        </div>
+    )
+}
+
+export default TaskCardHolder;
