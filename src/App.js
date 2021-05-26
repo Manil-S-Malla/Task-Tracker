2520598eaf838402d90667ac8e37a71394ba51cd
Manil S. Malla <manilsmalla@gmail.com>
2021-05-26 12:49:52 +0545

    Add 'TaskPanel' component to the 'App'.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import NavBar from './components/NavBar.component';
 import TaskCard from './components/TaskCard.component';
-
+import TaskPanel from './components/TaskPanel.component';
 
 import {ThemeProvider } from '@material-ui/core/styles';
-import {mainTheme} from './components/shared/Theme'
+import {mainTheme} from './components/Shared/Theme';
 
 
 function App() {
+  const ToDo = ['To-Do',[<TaskCard title= "Jogging"/>, <TaskCard title= "Coding"/> ]];
+  const InProgress = ['In Progress',[<TaskCard title= "Task Tracker Basic Frontend."/> ]];
+  const Done = ['Done',[<TaskCard title= "TT Design"/>, <TaskCard title= "TT Prem Backend"/>, <TaskCard title= "TT Basic components"/>]];
+  
   return(
     <ThemeProvider theme= {mainTheme}>
-      <NavBar />
-      <TaskCard title= "Jogging"/>
+      <NavBar/>
+      <div style= {{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <TaskPanel> 
+          {ToDo}
+          {InProgress}
+          {Done}
+        </TaskPanel>
+      </div>
     </ThemeProvider>
       
   )
