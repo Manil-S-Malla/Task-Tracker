bed5c4f17d120b623be744d2b6447f90841af359
Manil S. Malla <manilsmalla@gmail.com>
2021-05-28 12:43:26 +0545

    Update 'Colors'.

--- src/components/shared/Colors.js
+++ src/components/shared/Colors.js
@@ -4,7 +4,6 @@ const Colors = {
     background: '#FFFFFF',
     taskCardTitle: '#172B4D',
     taskCardSubTitle: '#757575',
-    //holder: '#F4F5F7',
     taskCardHolderTitle: '#900090',
     userAvatarBackground: '#03a9f4',
     userAvatarText: '#ffffff',
