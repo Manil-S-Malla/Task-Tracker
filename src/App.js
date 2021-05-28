a8a5b3bac681b997bf3cc2c1ff1e2279e843ec85
Manil S. Malla <manilsmalla@gmail.com>
2021-05-28 12:48:00 +0545

    Add React Router, Switch and Routes to the App.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 import './App.css';
 
-import NavBar from './components/NavBar.component';
-import TaskCard from './components/TaskCard.component';
-import TaskPanel from './components/TaskPanel.component';
+import HomePage from './components/Pages/Home';
+import LoginPage from './components/Pages/Login';
+import NotFoundPage from './components/Pages/NotFound';
 
 import {ThemeProvider } from '@material-ui/core/styles';
 import {mainTheme} from './components/Shared/Theme';
 
 
 function App() {
-  const ToDo = ['To-Do',[<TaskCard title= "Jogging"/>, <TaskCard title= "Coding"/> ]];
-  const InProgress = ['In Progress',[<TaskCard title= "Task Tracker Basic Frontend."/> ]];
-  const Done = ['Done',[<TaskCard title= "TT Design"/>, <TaskCard title= "TT Prem Backend"/>, <TaskCard title= "TT Basic components"/>]];
-  
-  return(
-    <ThemeProvider theme= {mainTheme}>
-      <NavBar/>
-      <div style= {{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-        <TaskPanel> 
-          {ToDo}
-          {InProgress}
-          {Done}
-        </TaskPanel>
-      </div>
-    </ThemeProvider>
-      
-  )
+    return(
+        <ThemeProvider theme= {mainTheme}>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+                    <Route exact path="/login">
+                        <LoginPage />
+                    </Route>
+                    <Route path="*">
+                        <NotFoundPage />
+                    </Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
+    )
 }
 
 export default App;
