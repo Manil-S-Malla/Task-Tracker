38e2b6a6a5cbada05161fd6c894691b6f04f8809
Manil S. Malla <manilsmalla@gmail.com>
2021-05-28 14:13:44 +0545

    Update Login Page.

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,17 +1,106 @@
+//  Review this file later. Not enough information.
+
 import React from 'react';
-import {Link} from "react-router-dom";
-import NavBar from '../NavBar.component';
+import {Link, useHistory} from "react-router-dom";
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import Style from '../Styles/LoginPage.Style';
+import Logo from '@material-ui/icons/TextFields';
+
 
 const Login = () => {
-    
-    return(
-        <React.Fragment>
-            <NavBar displayHeaderButtons= {false}/>
-            <div style= {{display: 'flex', justifyContent: 'center'}}>
-                <Link to="/">Home</Link>
-            </div>
-        </React.Fragment>
-    )
+    const classes = Style();
+
+    let history = useHistory();
+    const redirectHomepage = () => {
+        history.push('/')
+    }
+
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <Logo />
+                        </Avatar>
+
+                        <Typography component="h1" variant="h4">
+                            Task Tracker
+                        </Typography>
+
+                        <Typography component="h2" variant="h5">
+                            Login
+                        </Typography>
+                        
+                        <form className={classes.form} noValidate>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                                onClick= {() => {redirectHomepage()}}
+                            >
+                                Login
+                            </Button>
+
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link to="/login">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link to="/login">
+                                        Don't have an account? Sign Up
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </div>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default Login;
