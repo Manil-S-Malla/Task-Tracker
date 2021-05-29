690a6a69e33f99fe5c32f69b2d0875b3aeda6233
Manil S. Malla <manilsmalla@gmail.com>
2021-05-29 10:24:19 +0545

    Add backend url.

--- src/components/shared/CONST.js
+++ src/components/shared/CONST.js
@@ -1,4 +1,5 @@
-export const BaseURL = 'http://localhost:5000';
+export const BaseURL = 'http://localhost:3000';
+export const DBBaseURL = 'http://localhost:5000';
 
 export const TaskCardMaxWidth = 270;
 export const TaskCardHolderMaxWidth = '25vw';
