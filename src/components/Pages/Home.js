9429a9d6a47ea3fb4b6522c0dc79cafcb25db2fd
Manil S. Malla <manilsmalla@gmail.com>
2021-06-09 12:09:39 +0545

    Display user specific tasks.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,30 +1,82 @@
 import React from 'react';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+
+import axios from 'axios';
+import {DBBaseURL as DbUrl} from '../Shared/CONST';
 
 import NavBar from '../NavBar.component';
 import TaskCard from '../TaskCard.component';
 import TaskPanel from '../TaskPanel.component';
 
 const Home = () => {
-    
     useEffect(() => {
         document.title = "Task Tracker";
+        taskCardsSet();
     }, []);
 
-    const ToDo = ['To-Do',[<TaskCard title= "Jogging"/>, <TaskCard title= "Coding"/> ]];
-    const InProgress = ['In Progress',[<TaskCard title= "Task Tracker Basic Frontend."/> ]];
-    const Done = ['Done',[<TaskCard title= "TT Design"/>, <TaskCard title= "TT Prem Backend"/>, <TaskCard title= "TT Basic components"/>]];
+    const userId = useState(localStorage.getItem('userId'))[0];
+    const [taskCards, setTaskCards] = useState([]);
 
-    //console.log(username);
+    function getAllStatuses() {
+        const ApiURL = `${DbUrl}/status/`; 
+      
+        return axios.get(ApiURL)
+            .then(response => {
+                let statuses = [];
+
+                response.data.forEach(status => {
+                    statuses.push(status);
+                });
+
+                return statuses; 
+            })
+            .catch(error => null);
+    }
+
+    function getTasksByUserId(uId) {
+        const ApiURL = `${DbUrl}/task/user/${uId}`; 
+      
+        return axios.get(ApiURL)
+            .then(response => {
+                let tasks = [];
+                
+                response.data.forEach(task => {
+                    tasks.push(task);
+                });
+
+                return tasks; 
+            })
+            .catch(error => null);
+    }
+
+    function jsonToTaskCard(object){
+        return (
+            <TaskCard title= {object.name}/>
+        );
+    }
+
+    async function taskCardsSet() {
+        const statuses = await getAllStatuses();
+        const tasks = await getTasksByUserId(userId);
+        let taskPanelData = [];
+        
+        statuses.forEach(status => {
+            let tasksByStatus = tasks.filter(task => task.status === status._id);
+            let taskCardsByStatus = tasksByStatus.map(task => jsonToTaskCard(task));
+            taskPanelData.push([status, taskCardsByStatus]);
+        });
+
+        setTaskCards(taskPanelData);
+    }
 
     return(
         <React.Fragment>
             <NavBar/>
             <div style= {{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                 <TaskPanel> 
-                    {ToDo}
-                    {InProgress}
-                    {Done}
+                    {
+                        taskCards.map(taskCard => taskCard)
+                    }
                 </TaskPanel>
             </div>
         </React.Fragment>
