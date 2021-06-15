aa5a00b450fc32a78f02e0792159b40c180fa0f0
Manil S. Malla <manilsmalla@gmail.com>
2021-06-15 09:57:21 +0545

    Add Task Status Ids.

--- src/components/shared/CONST.js
+++ src/components/shared/CONST.js
@@ -5,6 +5,11 @@ export const TaskCardMaxWidth = 270;
 export const TaskCardHolderMaxWidth = '25vw';
 export const TaskCardHolderTitleHeight = 35;
 
+export const StatusIdToDo = '6086cb1ef719352a1cbf3af6';
+export const StatusIdInProgress = '6086cb53f719352a1cbf3af7';
+export const StatusIdDone = '6086cb90f719352a1cbf3af9';
+
+
 
 
 
