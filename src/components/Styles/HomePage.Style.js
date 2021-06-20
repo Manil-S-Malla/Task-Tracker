b5689f26a148eeb63258f1816f204000dfaf983a
Manil S. Malla <manilsmalla@gmail.com>
2021-06-20 13:06:44 +0545

    Stylize the home page.

---
+++ src/components/Styles/HomePage.Style.js
@@ -0,0 +1,22 @@
+import Colors from '../Shared/Colors';
+
+const AvatarWidth= '8vw';
+const LogoWidth= '4vw';
+
+const Styles = {
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+    },
+
+    body: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        position: 'relative',
+    },
+    
+}
+
+export default Styles;
