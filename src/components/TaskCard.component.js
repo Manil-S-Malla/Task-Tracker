842d2a04c70d8efd315fa2c2f87b7ea3b06d8d07
Manil S. Malla <manilsmalla@gmail.com>
2021-06-21 15:48:21 +0545

    Replace popup EditTask page with M-UI Dialog.

--- src/components/TaskCard.component.js
+++ src/components/TaskCard.component.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 
+import EditTask from './EditTask.component';
+
 import { ThemeProvider } from '@material-ui/core/styles';
 import Styles from './Styles/TaskCard.Style';
 import {editBlue} from './Shared/Theme';
@@ -14,6 +16,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Collapse from '@material-ui/core/Collapse';
+import Dialog from '@material-ui/core/Dialog';
 
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -29,6 +32,16 @@ const TaskCard = (props) => {
         setExpanded(!expanded);
     };
 
+    const [openEditDialogue, setOpenEditDialogue] = useState(false);
+
+    const handleOpenEditDialogue = () => {
+        setOpenEditDialogue(true);
+    };
+
+    const handleCloseEditDialogue = () => {
+        setOpenEditDialogue(false);
+    };
+
     const [timeTillDeadline, setTimeTillDeadline] = useState(new Date(props.hardDeadline).getTime() - new Date().getTime());
     
     useEffect(() => {
@@ -38,73 +51,88 @@ const TaskCard = (props) => {
     }, []);
 
     return (
-        <Card style = {Styles.root}>
-            <div style =  {Styles.cardHeader}>
-                <div style = {Styles.headerText}>
-                    <Typography style = {Styles.cardTitle}>{props.title}</Typography>
-                    <div style = {Styles.cardSubTitle}>
-                        {
-                            props.status === StatusIdDone ?
-                                '' :
-                                msToTimeTextShort(timeTillDeadline) === '' ?
-                                    'Deadline Passed' :
-                                    msToTimeTextShort(timeTillDeadline) + ' till deadline.'
-                        }
+        <React.Fragment>
+            <Card style = {Styles.root}>
+                <div style =  {Styles.cardHeader}>
+                    <div style = {Styles.headerText}>
+                        <Typography style = {Styles.cardTitle}>{props.title}</Typography>
+                        <div style = {Styles.cardSubTitle}>
+                            {
+                                props.status === StatusIdDone ?
+                                    '' :
+                                    msToTimeTextShort(timeTillDeadline) === '' ?
+                                        'Deadline Passed' :
+                                        msToTimeTextShort(timeTillDeadline) + ' till deadline.'
+                            }
+                        </div>
                     </div>
-                </div>
-                
-                <div style = {Styles.optionsButton}>
-                    <IconButton aria-label="Settings" color= 'primary'>
-                        <MoreVertIcon />
-                    </IconButton>
-                </div>
-            </div>
-            
-            <CardActions style = {Styles.cardActions}>
-               
-                    <div>
-                        <ThemeProvider theme= {editBlue}>
-                            <IconButton aria-label="Edit" color= 'primary'>
-                                <EditIcon/>
-                            </IconButton>
-                        </ThemeProvider>
-                        <IconButton aria-label="Go Behind">
-                            <UndoIcon />
-                        </IconButton>
-                        <IconButton aria-label="Go Ahead">
-                            <RedoIcon />
-                        </IconButton>
-                        <ThemeProvider theme= {deleteRed}>
-                            <IconButton aria-label="Delete"  color= 'primary'>
-                                <DeleteIcon />
-                            </IconButton>
-                        </ThemeProvider>
-                    </div>
-               
-                <div>
-                    <IconButton
-                        style = {expanded ? Styles.expanded : Styles.expand}
-                        onClick={handleExpand}
-                        aria-label="Show more"    
-                        color= 'primary'    
-                    >
-                        <ExpandMoreIcon />
-                    </IconButton>
-                </div>
-            </CardActions>
-            
-            <Collapse in={expanded}>
-                <CardContent>
-                
-                    <Typography paragraph>
-                        {props.description}
-                    </Typography>
                     
-                </CardContent>
-            </Collapse>
-            
-            
-        </Card>
+                    <div style = {Styles.optionsButton}>
+                        <IconButton aria-label="Settings" color= 'primary'>
+                            <MoreVertIcon />
+                        </IconButton>
+                    </div>
+                </div>
+                
+                <CardActions style = {Styles.cardActions}>
+                
+                        <div>
+                            <ThemeProvider theme= {editBlue}>
+                                <IconButton aria-label="Edit" color= 'primary' onClick={handleOpenEditDialogue}>
+                                    <EditIcon/>
+                                </IconButton>
+                            </ThemeProvider>
+                            <IconButton aria-label="Go Behind">
+                                <UndoIcon />
+                            </IconButton>
+                            <IconButton aria-label="Go Ahead">
+                                <RedoIcon />
+                            </IconButton>
+                            <ThemeProvider theme= {deleteRed}>
+                                <IconButton aria-label="Delete"  color= 'primary'>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </ThemeProvider>
+                        </div>
+                
+                    <div>
+                        <IconButton
+                            style = {expanded ? Styles.expanded : Styles.expand}
+                            onClick={handleExpand}
+                            aria-label="Show more"    
+                            color= 'primary'    
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </div>
+                </CardActions>
+                
+                <Collapse in={expanded}>
+                    <CardContent>
+                    
+                        <Typography paragraph>
+                            {props.description}
+                        </Typography>
+                        
+                    </CardContent>
+                </Collapse>                
+            </Card>
+
+            <Dialog open={openEditDialogue} onClose={handleCloseEditDialogue} aria-labelledby="form-dialog-title">
+                <EditTask 
+                    task= {{
+                        id: props.id, 
+                        name: props.title, 
+                        status: props.status,
+                        description: props.description,
+                        hardDeadline: props.hardDeadline,
+                        softDeadline: props.softDeadline,
+                    }}
+
+                    close= {handleCloseEditDialogue}
+                />
+            </Dialog>
+        </React.Fragment>
     );
 }
 
