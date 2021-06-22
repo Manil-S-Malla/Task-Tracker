19521665f019a267293623ff3eb77b435119ce09
Manil S. Malla <manilsmalla@gmail.com>
2021-06-22 15:49:01 +0545

    Stylize edit tasks.

--- src/components/Styles/EditTask.Style.js
+++ src/components/Styles/EditTask.Style.js
@@ -23,9 +23,74 @@ const Styles = {
         borderRadius: 5,
     },
 
+    container: {
+        margin: '10px 20px 10px 20px',   
+    },
+
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
     title: {
-        backgroundColor: Colors.background,
-    }
+        fontSize: 15,
+        fontWeight: '700',
+        color: Colors.editBlue,
+    },
+
+    subContainer1: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+
+    taskTitleHolder: {
+        flex: .8,
+        marginRight: 20,
+    },
+
+    formControl1: {
+        minWidth: 120, 
+        flex: .2, 
+        justifyContent: 'center',
+    },
+
+    taskDescriptionHolder: {
+        marginBottom: 30,
+    },
+        
+    subContainer2: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+
+    hardDeadline: {
+        flex: .5,
+        marginRight: 20,
+    },
+
+    softDeadline: {
+        flex: .5,
+    },
+
+    subContainer3: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+
+    done: {
+        border: '2px solid', 
+        marginBottom: 10
+    },
+
+    
 };
 
 export default Styles;
